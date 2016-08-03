78a0a648e65d287045e89e13f273dc86c7d38ca2
udaka21 <udaka21@gmail.com>
2016-08-03 14:18:37 +0530

    Added Comments and explained why ringBuffer.publish(sequence) put in to final

--- src/main/java/EventProducer.java
+++ src/main/java/EventProducer.java
@@ -1,27 +1,35 @@
 import com.lmax.disruptor.RingBuffer;
 
-
-public class EventProducer {
+class EventProducer {
     //initialize the ring buffer
     private final RingBuffer<Event> ringBuffer;
 
 
-    public EventProducer(RingBuffer<Event> ringBuffer) {
+    EventProducer(RingBuffer<Event> ringBuffer) {
         this.ringBuffer = ringBuffer;
     }
 
-    public void onData(String message) {
+    /**
+     * Put given messages to the Ring Buffer.
+     * @param message Message that put to the disruptor. Contains a single line of a .txt file.
+     */
+    void onData(String message) {
 
-        long sequence = ringBuffer.next();// Grab the next sequence
+        // Grab the next sequence
+        long sequence = ringBuffer.next();
 
         try {
-            Event event = ringBuffer.get(sequence);// Get the entry in the Disruptor
-            // for the sequence
-            event.setValue(message);// Fill with data
-
+            // Get the entry in the Disruptor for the sequence
+            Event event = ringBuffer.get(sequence);
+            // Fill with data
+            event.setValue(message);
         } finally {
+            /**
+             * Publish sequence whether value set or not in the ring buffer.
+             * This will help when there is an interrupt to set value to the ring buffer,
+             * But we want to continue the remaining process.
+             */
             ringBuffer.publish(sequence);
-
         }
 
     }
