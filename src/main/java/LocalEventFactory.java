a2397440550507c3b5e2edb1bb0f74885f5189e1
udaka21 <udaka21@gmail.com>
2016-08-03 14:24:11 +0530

    Made corrections in the comments

--- src/main/java/LocalEventFactory.java
+++ src/main/java/LocalEventFactory.java
@@ -1,8 +1,8 @@
 import com.lmax.disruptor.EventFactory;
 
 /**
- * In order to allow the Disruptor to preallocate these events for us,
- * We need to an EventFactory that will perform the construction
+ * In order to allow the Disruptor to preallocate these events,
+ * We need an EventFactory that will perform the construction.
  */
 public class LocalEventFactory implements EventFactory<Event> {
 
